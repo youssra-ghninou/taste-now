a3f3d40494e8218caa1695c69af9b2c892b448f4
youssra-ghninou <ghninou177u@gmail.com>
2022-09-14 17:30:59 +0100

    navbar added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,15 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'primary': '#F72D57',
+      'yellow': '#F2BC57',
+      'black': '#1E1E1E',
+    },
+    fontFamily: {
+      'praise': ['Praise', 'cursive'],
+      'poppins': ['Poppins'],
+    },
   },
   plugins: [],
 }
